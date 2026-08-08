fe55820f799ca23883d1c9337a8819326854ef1a
Asma P A <asmapa122002@gmail.com>
2026-08-08 14:25:16 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -95,39 +95,7 @@ const cartCount = cart.reduce((total, item) => {
       <a href="#">Contact</a>
     </div>
 
-    <div className="mt-3 border-t border-pink-100 pt-3 px-3">
-  <div className="grid grid-cols-4 gap-2 text-center">
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700">
-      Necklace
-    </button>
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700">
-      Rings
-    </button>
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700">
-      Earrings
-    </button>
-
-    <button className="rounded-full text-pink-500  py-2 text-[11px] font-medium text-gray-700">
-      Bracelets
-    </button>
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700">
-      Bangles
-    </button>
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700">
-      Chains
-    </button>
-
-    <button className="rounded-full text-pink-500 py-2 text-[11px] font-medium text-gray-700 col-span-2">
-      Gift Sets
-    </button>
-
-  </div>
-</div>
+    
 
     
     
